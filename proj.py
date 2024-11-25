d30d1dccc8e372e275d6cacb0c792befb29feef6
GOPSTNM <166233545+GOPSTNM@users.noreply.github.com>
2024-11-25 21:03:40 +0800

    Update proj.py

--- proj.py
+++ proj.py
@@ -1,8 +1,25 @@
 import os
 import random
 def sub1(): 
-  print("Write something...") 
-  input()
+  num = random.randint(2, 99)
+  #lower value
+  lv = 1
+  #higher value
+  uv = 100
+  userinput = 0
+  while (userinput != num):
+    correct = 0
+    while (correct = 0):
+      userinput = input("Guess a number between " + str(lv) + " and " + str(uv) + ":")
+      if (userinput >= uv) or (userinput <= lv):
+        print("Input out of range, please input again.")
+      else:
+        correct = 1
+    if (userinput > num):
+      uv = userinput
+    if (userinput < num):
+      lv = userinput
+  print("Bingo")
 def sub2():
   print("Write something...") 
   input()
