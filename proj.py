9e4a651907a7178c38a7d95928bc984aa3f09dc1
GOPSTNM <166233545+GOPSTNM@users.noreply.github.com>
2024-11-25 21:06:44 +0800

    ?

--- proj.py
+++ proj.py
@@ -1,6 +1,6 @@
 import os
 import random
-def sub1(): 
+def sub1():
   num = random.randint(2, 99)
   #lower value
   lv = 1
@@ -21,30 +21,30 @@ def sub1(): 
       lv = userinput
   print("Bingo")
 def sub2():
-  print("Write something...") 
+  print("Write something...")
   input()
-def bonus(): 
-  print("Write something...") 
-  input() 
-if __name__ == "__main__": 
-  choice = '' 
+def bonus():
+  print("Write something...")
+  input()
+if __name__ == "__main__":
+  choice = ''
   while (choice!='E'):
     os.system('cls')
-    print('**********************************************************') 
-    print('**************************YLMASS**************************') 
-    print('************************Bingo Game************************') 
-    print('*****************A.Easy Level*****************************') 
+    print('**********************************************************')
+    print('**************************YLMASS**************************')
+    print('************************Bingo Game************************')
+    print('*****************A.Easy Level*****************************')
     print('*****************B.Difficult Level ***********************')
-    print('*****************C.Challenging Level *********************') 
+    print('*****************C.Challenging Level *********************')
     print('*****************D.Exit **********************************')
-    choice = input("Input a choice (A,B,C,D):    ") 
-    while ((choice<'A') or (choice>'D')): 
+    choice = input("Input a choice (A,B,C,D):    ")
+    while ((choice<'A') or (choice>'D')):
       print("Input out of range, please input again.")
-      choice = input("Input a choice (A,B,C,D):    ") 
-    if choice == 'A': 
-      sub1() 
-    if choice == 'B': 
-      sub2() 
-    if choice == 'C': 
-      bonus() 
+      choice = input("Input a choice (A,B,C,D):    ")
+    if choice == 'A':
+      sub1()
+    if choice == 'B':
+      sub2()
+    if choice == 'C':
+      bonus()
     print('*************************Bye Bye!****************************')
