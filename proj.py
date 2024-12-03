83937aae1f549099c147118d1d323ea9576620cd
GOPSTNM <166233545+GOPSTNM@users.noreply.github.com>
2024-12-03 09:12:45 +0800

    Update proj.py

--- proj.py
+++ proj.py
@@ -65,7 +65,7 @@ def bonus():
     if (userinput < num):
       lv = userinput
   print("Bingo")
-  print("You used " + str(int(time.time()-start_time)) + " to guess the number")
+  print("You used " + str(int(time.time()-start_time)) + "s to guess the number")
 if __name__ == "__main__":
   choice = ''
   while (choice!='E'):
